ff4d5fe45be3f2cefaa788d52b1508d1c1c78fe5
phj0609 <jua06067@naver.com>
2022-07-07 15:45:19 +0900

    댓글페이지처리

---
+++ src/main/java/me/light/model/Board.java
@@ -0,0 +1,29 @@
+package me.light.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board {
+	private Long bno; 
+	private String title;  
+	private String content;  
+	private String writer;  
+	private LocalDateTime regDate; 
+	private LocalDateTime updateDate;
+	
+	private int replyCnt;
+	
+	// 게시글(1) -> 댓글(n)
+	private List<Reply> replyList;
+}
